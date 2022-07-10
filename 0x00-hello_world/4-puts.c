71ee05cacd0afe9ac191602d69b58bed8beed99c
CarolShiko <mbuguawanjiku208@gmail.com>
2022-07-10 09:10:00 -0700

    Hello World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
 /*main -Entry point*/ 
 /*Return:Always O (sucess)*/
-
 int main (void)
 {
-puts("\"Programing is like building a multilingual puzzle"\");
+puts("\""Programing is like building a multilingual puzzle"\");
 return (0);
 }

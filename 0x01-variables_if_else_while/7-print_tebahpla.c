43a084df59fd4306f9b5fcac5634856751eb446c
CarolShiko <mbuguawanjiku208@gmail.com>
2022-07-15 08:57:43 -0700

    characters

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print in lowercasealphabets in reverse mode.
+ *
+ * Return: always 0.
+ */
+int main(void)
+{ 
+	char letter;
+
+	for (letter = 'z'; letter <= 'a'; letter++)
+		putchar (letter)
+
+	putchar('\n')
+
+	return (0);
+}
